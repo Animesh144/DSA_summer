b755ff7da93d2929d0041070887568336003e398
Animesh kumar ojha <ojhaanimesh2004@gmail.com>
2025-06-25 17:14:43 +0530

    day15

--- day15/KthSmallestElementInBST_230.java
+++ day15/KthSmallestElementInBST_230.java
@@ -1,5 +1,100 @@
 package day15;
 
+import java.util.*;
+
 public class KthSmallestElementInBST_230 {
-    
+    static int i = 0;
+
+    static int res = 0;
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        TreeNode root = null;
+        while (true) {
+            int val = read.nextInt();
+            if (val == -1)
+                break;
+            root = insertionInBST(root, val);
+        }
+
+        int k = read.nextInt();
+        read.close();
+
+        KthSmallestElementInBST_230 obj = new KthSmallestElementInBST_230();
+        int result = obj.kthSmallest(root, k);
+        System.out.println(result);
+    }
+
+    public static TreeNode insertionInBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+        if (val < root.val)
+            root.left = insertionInBST(root.left, val);
+        else
+            root.right = insertionInBST(root.right, val);
+        return root;
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return res;
+    }
+
+    public void inorder(TreeNode root, int k) {
+        if (root == null)
+            return;
+        kthSmallest(root.left, k);
+        i++;
+        if (i == k)
+            res = root.val;
+        kthSmallest(root.right, k);
+    }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+// class Solution {
+// int i = 0;
+// int res = 0;
+
+// public int kthSmallest(TreeNode root, int k) {
+// inorder(root, k);
+// return res;
+// }
+
+// public void inorder(TreeNode root, int k) {
+// if (root == null)
+// return;
+// kthSmallest(root.left, k);
+// i++;
+// if (i == k)
+// res = root.val;
+// kthSmallest(root.right, k);
+// }
+// }
--- day15/maximumSumBSTinBT_1373.java
+++ day15/maximumSumBSTinBT_1373.java
@@ -1,24 +1,93 @@
 package day15;
+
 import java.util.*;
+
 public class maximumSumBSTinBT_1373 {
-    public static void main(String[] args){
+    class unit {
+        boolean bst;
+        int sum;
+        int max, min;
+
+        unit(boolean bst, int sum, int min, int max) {
+            this.bst = bst;
+            this.sum = sum;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    int Msum = 0;
+
+    public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        maximumSumBSTinBT_1373 obj = new maximumSumBSTinBT_1373();
+        TreeNode root = null;
+
+        System.out.println("Enter number of nodes:");
+        int n = read.nextInt();
+        System.out.println("Enter " + n + " node values (use -1 to skip a node):");
+
+        for (int i = 0; i < n; i++) {
+            int val = read.nextInt();
+            if (val != -1) {
+                root = insertionInBST(root, val);
+            }
+        }
+
+        int result = obj.maximumSumBSTinBTSolution(root);
+        System.out.println("Maximum Sum BST in Binary Tree: " + result);
         read.close();
     }
-    public static TreeNode insertionInBST(TreeNode root, int val){
-        if(root == null) return new TreeNode(val);
-        if(val < root.val) root.left = insertionInBST(root.left, val);
-        else root.right = insertionInBST (root.right, val);
+
+    public int maximumSumBSTinBTSolution(TreeNode root) {
+        postorder(root);
+        return Msum;
+    }
+
+    public static TreeNode insertionInBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+        if (val < root.val)
+            root.left = insertionInBST(root.left, val);
+        else
+            root.right = insertionInBST(root.right, val);
         return root;
     }
+
+    public unit postorder(TreeNode curr) {
+        if (curr == null) {
+            return new unit(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        if (curr.left == null && curr.right == null) {
+            if (curr.val > Msum)
+                Msum = curr.val;
+            return new unit(true, curr.val, curr.val, curr.val);
+        }
+        unit l = postorder(curr.left);
+        unit r = postorder(curr.right);
+
+        if (l.bst == true && r.bst == true && curr.val > l.max && curr.val < r.min) {
+            int tempsum = l.sum + r.sum + curr.val;
+            if (Msum < tempsum)
+                Msum = tempsum;
+            int m = (curr.left == null) ? curr.val : l.min;
+            int M = (curr.right == null) ? curr.val : r.max;
+            return new unit(true, tempsum, m, M);
+        } else {
+            return new unit(false, Math.max(l.sum, r.sum), 0, 0);
+        }
+
+    }
 }
-class TreeNode{
+
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int val){
+
+    TreeNode(int val) {
         this.val = val;
-        this.left = null; 
+        this.left = null;
         this.right = null;
     }
 }
